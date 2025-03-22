d1305804b5a5f6b13f90fb6d4e446f1dbef3f5aa
rupanjan-chakraborty <rupanjan.chakraborty1@gmail.com>
2025-03-22 19:37:03 +0530

    Added changes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,144 @@
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "My-VPC"
+  }
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "public-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.demo-igw.id
+}
+
+resource "aws_route_table_association" "public-subnet-1-association" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "public-subnet-2-association" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_internet_gateway" "demo-igw" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "My-VPC-IGW"
+  }
+}
+
+resource "aws_eip" "nat-eip" {
+  domain = "vpc"
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.nat-eip.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_security_group" "web-sg" {
+  vpc_id = aws_vpc.my-vpc.id
+  name   = "web-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_security_group" "db-sg" {
+  vpc_id = aws_vpc.my-vpc.id
+  name   = "db-sg"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+    # Allow traffic from private subnets
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db-sg"
+  }
+}
+
 resource "aws_instance" "servers" {
   for_each      = var.instances
   ami           = each.value.ami
   instance_type = each.value.instance_type
+  subnet_id     = aws_subnet.public-subnet-1
+  key_name      = "test-key"
   tags = {
     Name = "${each.key}"
   }
@@ -11,3 +148,18 @@ resource "aws_instance" "servers" {
     delete_on_termination = true
   }
 }
+
+# resource "aws_key_pair" "TF_key" {
+#   key_name   = "TF_key"
+#   public_key = tls_private_key.rsa.public_key_openssh
+# }
+
+# resource "tls_private_key" "rsa" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
+
+# resource "local_file" "TF-key" {
+#     content  = tls_private_key.rsa.private_key_pem
+#     filename = "tfkey"
+# }
