1cce77e656345d6ee8a18a7bff3f265e31952f5f
rupanjan-chakraborty <rupanjan.chakraborty1@gmail.com>
2025-03-24 16:43:55 +0530

    Added changes

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -6,23 +6,3 @@ variable "instances" {
     subnet_id     = string
   }))
 }
-
-# variable "private-subnet-1_id" {}
-# variable "private-subnet-2_id" {}
-# variable "public-subnet-1_id" {}
-# variable "public-subnet-2_id" {}
-
-# locals {
-#   # depends_on = [
-#   #   aws_subnet.private-subnet-1, 
-#   #   aws_subnet.private-subnet-2, 
-#   #   aws_subnet.public-subnet-1, 
-#   #   aws_subnet.public-subnet-1
-#   #   ]
-#   subnet_ids = [
-#     data.aws_subnet.public-subnet-1.id,
-#     data.aws_subnet.private-subnet-1.id,
-#     data.aws_subnet.public-subnet-2.id,
-#     data.aws_subnet.private-subnet-2.id,
-#   ]
-# }
