19a04f436031c75dbc169689ac859a2845f90609
rupanjan-chakraborty <rupanjan.chakraborty1@gmail.com>
2026-04-14 16:13:12 +0530

    Test CI/CD working

--- DevOps/app.js
+++ DevOps/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Kubernetes!');
+  res.end('Hello from CI/CD 🚀 FINAL');
 });
 
 server.listen(3000, '0.0.0.0', () => {
